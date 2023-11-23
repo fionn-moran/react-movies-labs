ae8c31875a526862a356ed060bfe793a0c62db25
Fionn Moran <fionnmoran13@gmail.com>
2023-11-23 21:27:28 +0000

    Fix upcoming movies page.

--- movies/src/index.js
+++ movies/src/index.js
@@ -31,7 +31,7 @@ const App = () => {
         <Routes>
           <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
           <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-          {/* <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} /> */}
+          <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
           <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
           <Route path="/movies/:id" element={<MoviePage />} />
           <Route path="/" element={<HomePage />} />
